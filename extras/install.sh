4bb8b4567d8d0a5703c9bb874dc7aa18af089f33
Aravinda Vishwanathapura <aravinda@kadalu.tech>
2023-02-18 15:06:55 +0530

    Fix the install script
    Signed-off-by: Aravinda Vishwanathapura <aravinda@kadalu.tech>

--- extras/install.sh
+++ extras/install.sh
@@ -7,9 +7,9 @@ curl -fsSL https://github.com/kadalu/volgen/releases/latest/download/shd.vol.j2 
 curl -fsSL https://github.com/kadalu/volgen/releases/latest/download/__init__.py -o /tmp/__init__.py
 curl -fsSL https://github.com/kadalu/volgen/releases/latest/download/setup.py -o /tmp/setup.py
 
-install -m 700 /tmp/client.vol.j2 /var/lib/kadalu/templates/
-install -m 700 /tmp/storage_unit.vol.j2 /var/lib/kadalu/templates/
-install -m 700 /tmp/shd.vol.j2 /var/lib/kadalu/templates/
+install -D -m 700 /tmp/client.vol.j2 /var/lib/kadalu/templates/client.vol.j2
+install -D -m 700 /tmp/storage_unit.vol.j2 /var/lib/kadalu/templates/storage_unit.vol.j2
+install -D -m 700 /tmp/shd.vol.j2 /var/lib/kadalu/templates/shd.vol.j2
 install /tmp/kadalu-volgen /usr/bin/kadalu-volgen
 mkdir -p /tmp/python/kadalu_volgen
 mv /tmp/__init__.py /tmp/python/kadalu_volgen/
